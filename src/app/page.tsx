be85885b674bf08f1f8dcccd17806dd2bd56c5a2
YanGidorini <ygidorini@gmail.com>
2024-06-25 15:25:48 -0300

    fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default async function Home() {
   let result: number = 0;
   try {
     const url = process.env.ENV_BASE_URL
-      ? `${process.env.ENV_BASE_URL}/api/pages`
-      : "https://localhost:3000/api/pages";
+      ? `${process.env.ENV_BASE_URL}/api/pages/home`
+      : "http://localhost:3000/api/pages/home";
 
     const response = await fetch(url, {
       next: { revalidate: 30 },
@@ -18,7 +18,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Number: ${result}</h1>
+      <h1>Number: {result}</h1>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
